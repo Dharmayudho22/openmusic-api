058e39a70a8e7cc6812278089b335b18a26d8993
dharmayudho <sudarmayudhoprayitno@gmail.com>
2025-06-08 09:23:25 +0700

    Inisialisasi proyek OpenMusic API

---
+++ src/server/index.js
@@ -0,0 +1,84 @@
+const Hapi = require('@hapi/hapi');
+const dotenv = require('dotenv');
+//const { validateSongPayload } = require('../validator/songsValidator');
+
+dotenv.config();
+
+const init = async() => {
+  const server = Hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  //routes
+  const albumRoutes = require('../routes/albums');
+  const songRoutes = require('../routes/songs');
+  server.route([...albumRoutes, ...songRoutes]);
+    
+  //error
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response.isBoom) {
+      if (response.output.statusCode === 404) {
+        return h.response({
+          status: 'fail',
+          message: 'Resource tidak ditemukan',
+        }).code(404);
+      }
+
+      if (response.isClientError) {
+        return h.response({
+          status: 'fail',
+          message: response.message,
+        }).code(response.output.statusCode);
+      }
+
+      console.error(response);
+      return h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      }).code(500);
+    }
+
+    // Tangani error custom (seperti NotFoundError, ValidationError)
+    if (response instanceof Error) {
+      if (response.name === 'NotFoundError') {
+        return h.response({
+          status: 'fail',
+          message: response.message,
+        }).code(404);
+      }
+
+      if (response.name === 'ValidationError') {
+        return h.response({
+          status: 'fail',
+          message: response.message,
+        }).code(400);
+      }
+
+      console.error(response);
+      return h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      }).code(500);
+    }
+
+    return h.continue;
+  });
+
+  console.log('Registered Routes:');
+  server.table().forEach(route => {
+  console.log(`- ${route.method.toUpperCase()} ${route.path}`);
+  });
+
+  await server.start();
+  console.log(`Server will run at http://${process.env.HOST}:${process.env.PORT}`);
+};
+
+init();
